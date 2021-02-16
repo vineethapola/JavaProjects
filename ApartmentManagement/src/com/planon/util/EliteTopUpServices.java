cf5d0052bdcd3ef38f5786b2bec89e9144fc309f
Vineetha Pola <Vineetha.Pola@planonsoftware.com>
2021-02-16 11:17:38 +0530

    Added documentation for all the classes and interfaces

--- ApartmentManagement/src/com/planon/util/EliteTopUpServices.java
+++ ApartmentManagement/src/com/planon/util/EliteTopUpServices.java
@@ -5,6 +5,9 @@ import java.util.logging.Logger;
 public class EliteTopUpServices implements TopUpServices {
 	private static final Logger log = Logger.getLogger(EliteTopUpServices.class.getName());
 
+	/**
+	 * Constructor which tells the user  to choose from
+	 */
 	public EliteTopUpServices() {
 		log.info("Power backup");
 		log.info("Second car parking");
